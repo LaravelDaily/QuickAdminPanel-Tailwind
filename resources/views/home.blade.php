742b75d9638f2df974f31167d680c62b2c248692
Marius Kasparavičius <marre124@gmail.com>
2020-09-14 09:46:50 +0300

    Homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,24 +1,18 @@
 @extends('layouts.admin')
 @section('content')
-<div class="content">
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="card">
-                <div class="card-header">
-                    Dashboard
-                </div>
+<div class="max-w w-full bg-white shadow-md rounded-md overflow-hidden border">
+    <div class="flex justify-between items-center px-5 py-3 text-gray-700 border-b">
+        <h3 class="text-sm">Dashboard</h3>
+    </div>
 
-                <div class="card-body">
-                    @if(session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="px-5 py-6 bg-gray-200 text-gray-700 border-b">
+        @if(session('status'))
+            <div class="flex max-w w-full bg-green-300 shadow-md rounded-lg overflow-hidden mb-4 py-4 px-6 text-green-700 font-medium">
+                {{ session('status') }}
             </div>
-        </div>
+        @endif
+
+        You are logged in!
     </div>
 </div>
 @endsection
